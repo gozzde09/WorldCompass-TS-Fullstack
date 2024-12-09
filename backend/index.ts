6333bd669342b5b6f7f271d6f81539c967794e25
Gzd Akgn <gozzde0990@gmail.com>
2024-12-09 23:12:00 +0100

    Database uppdate

--- backend/index.ts
+++ backend/index.ts
@@ -7,12 +7,12 @@ import path from "path";
 import { Country } from "./types/interfaces";
 import { User } from "./types/interfaces";
 import { Status } from "./types/interfaces";
-import { BucketList } from "./types/interfaces";
+import { TravelList } from "./types/interfaces";
 
 const app = express();
 app.use(express.json());
 app.use(cors());
-require("dotenv").config();
+dotenv.config();
 
 const client = new Client({
   connectionString: process.env.PGURI,
@@ -55,6 +55,22 @@ app.get("/api/countries", async (_req: Request, res: Response) => {
     res.status(500).json(error + "Error fetching countries");
   }
 });
+// GET - ett land
+app.get("/api/countries/:name", async (req: Request, res: Response) => {
+  const { name } = req.params;
+  try {
+    const result = await client.query(
+      "SELECT * FROM countries WHERE country_name = $1",
+      [name]
+    );
+    if (result.rows.length === 0) {
+      res.status(404).json({ error: "Country not found" });
+    }
+    res.json(result.rows[0]);
+  } catch (error) {
+    res.status(500).json({ error: "Failed to fetch country details" });
+  }
+});
 
 // POST - Lägg till länder
 app.post("/api/countries", async (req: Request, res: Response) => {
@@ -66,12 +82,13 @@ app.post("/api/countries", async (req: Request, res: Response) => {
     country_continent,
     country_language,
     country_currency,
+    country_flag,
   }: Country = req.body;
 
   try {
     const query = `
-      INSERT INTO countries (country_name, country_description, country_capital, country_population, country_continent, country_language, country_currency)
-      VALUES ($1, $2, $3, $4, $5, $6, $7)
+      INSERT INTO countries (country_name, country_description, country_capital, country_population, country_continent, country_language, country_currency, country_flag)
+      VALUES ($1, $2, $3, $4, $5, $6, $7, $8)
       RETURNING *;
     `;
     const values = [
@@ -82,6 +99,7 @@ app.post("/api/countries", async (req: Request, res: Response) => {
       country_continent,
       country_language,
       country_currency,
+      country_flag,
     ];
     const { rows } = await client.query<Country>(query, values);
     res.status(201).send(rows[0]);
@@ -100,20 +118,19 @@ app.get("/api/visitstatus", async (_req: Request, res: Response) => {
   }
 });
 
-// GET - bucketlist med alla tabeller
-app.get("/api/bucketlist", async (_req: Request, res: Response) => {
+// GET - travellist med alla tabeller
+app.get("/api/travellist", async (_req: Request, res: Response) => {
   try {
-    const { rows } = await client.query<BucketList>(
+    const { rows } = await client.query<TravelList>(
       `SELECT
-          bucketlist.bucketlist_id,
+          travellist.travellist_id,
           users.first_name,
           countries.country_name,
           visitstatus.status_name,
-          bucketlist.notes
-      FROM bucketList
-      JOIN users ON bucketlist.user_id = users.user_id
-      JOIN countries ON bucketlist.country_id = countries.country_id
-      JOIN visitstatus ON bucketlist.status_id = visitstatus.status_id;`
+      FROM travellist
+      JOIN users ON travellist.user_id = users.user_id
+      JOIN countries ON travellist.country_id = countries.country_id
+      JOIN visitstatus ON travellist.status_id = visitstatus.status_id;`
     );
     res.status(200).json(rows);
   } catch (error) {
@@ -121,60 +138,60 @@ app.get("/api/bucketlist", async (_req: Request, res: Response) => {
   }
 });
 
-// POST - Lägg land till bucketlist
-app.post("/api/bucketlist", async (req: Request, res: Response) => {
-  const { country_id, status_id, user_id, notes }: BucketList = req.body;
+// POST - Lägg land till travellist
+app.post("/api/travellist", async (req: Request, res: Response) => {
+  const { country_id, status_id, user_id }: TravelList = req.body;
   try {
     const query = `
-      INSERT INTO bucketlist (country_id, status_id, user_id, notes)
+      INSERT INTO travellist (country_id, status_id, user_id)
       VALUES ($1, $2, $3, $4)
       RETURNING *;
     `;
-    const values = [country_id, status_id, user_id, notes];
-    const { rows } = await client.query<BucketList>(query, values);
+    const values = [country_id, status_id, user_id];
+    const { rows } = await client.query<TravelList>(query, values);
     res.status(201).send(rows[0]);
   } catch (error) {
-    res.status(500).json({ error: "Error adding country to the bucketlist" });
+    res.status(500).json({ error: "Error adding country to the travellist" });
   }
 });
 
-// PUT - Uppdatera status i bucketlist
+// PUT - Uppdatera status i travellist
 app.put(
-  "/api/bucketlist/:id",
+  "/api/travellist/:id",
   async (req: Request<{ id: string }>, res: Response) => {
     const { id } = req.params;
     const { status_id }: Status = req.body;
 
     try {
-      const { rows } = await client.query<BucketList>(
-        "UPDATE Bucketlist SET status_id = $1 WHERE bucketlist_id  = $2 RETURNING *",
+      const { rows } = await client.query<TravelList>(
+        "UPDATE travellist SET status_id = $1 WHERE travellist_id  = $2 RETURNING *",
         [status_id, id]
       );
       res.status(200).send(rows[0]);
     } catch (error) {
       res
         .status(500)
-        .json(error + "Error updating the visitstatus in the bucketlist");
+        .json(error + "Error updating the visitstatus in the travellist");
     }
   }
 );
 
-// DELETE - Ta bort ett land från bucketlist
+// DELETE - Ta bort ett land från travellist
 app.delete(
-  "/api/bucketlist/:id",
+  "/api/travellist/:id",
   async (req: Request<{ id: string }>, res: Response) => {
     const { id } = req.params;
 
     try {
-      await client.query<BucketList>(
-        `DELETE FROM Bucketlist WHERE bucketlist_id = $1 RETURNING *`,
+      await client.query<TravelList>(
+        `DELETE FROM travellist WHERE travellist_id = $1 RETURNING *`,
         [id]
       );
       res.status(204).send();
     } catch (error) {
       res
         .status(500)
-        .json(error + "Error deleting the country from the bucketlist");
+        .json(error + "Error deleting the country from the travellist");
     }
   }
 );
--- init.sql
+++ init.sql
@@ -1,4 +1,4 @@
-DROP TABLE IF EXISTS bucketlist;
+DROP TABLE IF EXISTS travellist;
 DROP TABLE IF EXISTS countries;
 DROP TABLE IF EXISTS visitstatus;
 DROP TABLE IF EXISTS users;
@@ -6,13 +6,14 @@ DROP TABLE IF EXISTS users;
 -- 1. Countries Table
 CREATE TABLE countries (
     country_id SERIAL PRIMARY KEY,
-    country_name VARCHAR(50) UNIQUE NOT NULL,
+    country_name VARCHAR(100) UNIQUE NOT NULL,
     country_description TEXT,
-    country_capital VARCHAR(50),
+    country_capital VARCHAR(100),
     country_population INT,
-    country_continent VARCHAR(50),
-    country_language VARCHAR(50),
-    country_currency VARCHAR(50)
+    country_continent VARCHAR(100),
+    country_language VARCHAR(100),
+    country_currency VARCHAR(100),
+    country_flag TEXT
 );
 
 -- 2. Status Table
@@ -30,21 +31,20 @@ CREATE TABLE users (
     password VARCHAR(50) NOT NULL
 );
 
--- 4. Bucket List Table
-CREATE TABLE bucketlist (
-    bucketlist_id SERIAL PRIMARY KEY,
+-- 4. Travel List Table
+CREATE TABLE travellist (
+    travellist_id SERIAL PRIMARY KEY,
     country_id INT REFERENCES countries(country_id),
     status_id INT REFERENCES visitstatus(status_id),
     user_id INT REFERENCES users(user_id),
-    notes TEXT,
     UNIQUE (country_id, status_id, user_id)
 );
 
-INSERT INTO countries (country_name, country_description, country_capital, country_population, country_continent, country_language, country_currency)
-VALUES
-('Japan', 'A country known for its rich culture, technology, and cherry blossoms.', 'Tokyo', 125800000, 'Asia', 'Japanese', 'Yen'),
-('Sweden', 'A Scandinavian nation known for its forests and advanced social systems.', 'Stockholm', 10350000, 'Europe', 'Swedish', 'Krona'),
-('USA', 'A large and diverse country known for its influence on world culture and economy.', 'Washington D.C.', 331000000, 'North America', 'English', 'Dollar');
+-- INSERT INTO countries (country_name, country_description, country_capital, country_population, country_continent, country_language, country_currency)
+-- VALUES
+-- ('Japan', 'A country known for its rich culture, technology, and cherry blossoms.', 'Tokyo', 125800000, 'Asia', 'Japanese', 'Yen'),
+-- ('Sweden', 'A Scandinavian nation known for its forests and advanced social systems.', 'Stockholm', 10350000, 'Europe', 'Swedish', 'Krona'),
+-- ('USA', 'A large and diverse country known for its influence on world culture and economy.', 'Washington D.C.', 331000000, 'North America', 'English', 'Dollar');
 
 INSERT INTO visitstatus (status_name)
 VALUES
@@ -53,24 +53,22 @@ VALUES
 
 INSERT INTO users (first_name, last_name, email, password)
 VALUES
-('Alice', 'Johnson', 'alice@example.com', 'hashedpassword1'),
-('Bob', 'Smith', 'bob@example.com', 'hashedpassword2'),
-('Charlie', 'Brown', 'charlie@example.com', 'hashedpassword3');
+('Alice', 'Johnson', 'alice@example.com', 'password1'),
+('Bob', 'Smith', 'bob@example.com', 'password2'),
+('Charlie', 'Brown', 'charlie@example.com', 'password3');
 
+-- INSERT INTO travellist (country_id, status_id, user_id)
+-- VALUES
+-- (1, 1, 1),   -- country_id = 1 (Japan), status_id = 1 (Want to visit), user_id = 1 (Alice)
+-- (2, 2, 2),   -- country_id = 2 (Sweden), status_id = 2 (Visited), user_id = 2 (Bob)
+-- (3, 1, 3);
 
-INSERT INTO bucketlist (country_id, status_id, user_id, notes)
-VALUES
-(1, 1, 1, 'Japan is a must-see destination!'),   -- country_id = 1 (Japan), status_id = 1 (Want to visit), user_id = 1 (Alice)
-(2, 2, 2, 'Visited Stockholm last summer.'),    -- country_id = 2 (Sweden), status_id = 2 (Visited), user_id = 2 (Bob)
-(3, 1, 3, 'Planning to visit the USA next year.');
-
-SELECT
-    bucketlist.bucketlist_id,
-    users.first_name,
-    countries.country_name,
-    visitstatus.status_name,
-    bucketlist.notes
-FROM bucketList
-JOIN users ON bucketlist.user_id = users.user_id
-JOIN countries ON bucketlist.country_id = countries.country_id
-JOIN visitstatus ON bucketlist.status_id = visitstatus.status_id;
+-- SELECT
+--     travellist.travellist_id,
+--     users.first_name,
+--     countries.country_name,
+--     visitstatus.status_name
+-- FROM travellist
+-- JOIN users ON travellist.user_id = users.user_id
+-- JOIN countries ON travellist.country_id = countries.country_id
+-- JOIN visitstatus ON travellist.status_id = visitstatus.status_id;
