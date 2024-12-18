206323b7288a4dd59473f21484e45717b3d95ed2
Gzd Akgn <gozzde0990@gmail.com>
2024-12-18 21:29:17 +0100

    NavBar

---
+++ frontend/src/components/NavBar.tsx
@@ -0,0 +1,28 @@
+import { Link } from "react-router-dom";
+import { Navbar as BootstrapNavbar, Nav, Container } from "react-bootstrap";
+
+export default function Navbar() {
+  const handleLogout = () => {
+    localStorage.removeItem("userId");
+  };
+  return (
+    <BootstrapNavbar
+      style={{ backgroundColor: "#3d3b8e" }}
+      expand='lg'
+      variant='dark'>
+      <Container>
+        <BootstrapNavbar.Brand as={Link} to='/'>
+          World Compass
+        </BootstrapNavbar.Brand>
+        <BootstrapNavbar.Toggle aria-controls='basic-navbar-nav' />
+        <BootstrapNavbar.Collapse id='basic-navbar-nav'>
+          <Nav className='ms-auto'>
+            <Nav.Link as={Link} to='/' onClick={handleLogout}>
+              Log out
+            </Nav.Link>
+          </Nav>
+        </BootstrapNavbar.Collapse>
+      </Container>
+    </BootstrapNavbar>
+  );
+}
